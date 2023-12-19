26769da43de440ae5dbe0bcfbdc872645f89fc5e
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-12-19 17:00:19 +0300

    email verification schema

---
+++ models/emailVerificationModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const emailVerificationSchema = new mongoose.Schema({
+  userId: String,
+  token: String,
+  createdAt: Date,
+  expiresAt: Date,
+});
+
+const Verification = mongoose.model("Verification", emailVerificationSchema);
+
+export default Verification;
